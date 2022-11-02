aca639368d0b613988a5ba5a6778e0d6f293462d
Maja Gawlikowska <majgaw@onet.eu>
2022-11-02 13:44:29 +0100

    function destroyed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 console.log("Hi there, (❁´◡`❁)");
 
-const button = document.querySelector(".button");
-const footer__header = document.querySelector(".footer__header");
+{
+const TaggleEmail = () => {
+   footer__header.innerText ("e-mail") === button.innerText ("Pokaż") = footer__header.innerText ("maja@poczta.pl") === button.innerText ("Ukryj")
+   footer__header.innerText ("maja@poczta.pl") === button.innerText ("Ukryj") = footer__header.innerText ("e-mail") === button.innerText ("Pokaż")
+}
+
+const init = () => {
+button = document.querySelector(".button");
+button.addEventListener ("click" TaggleEmail);
+
+}
+}
+
+
+//tak było: 
+let button = document.querySelector(".button");
+let footer__header = document.querySelector(".footer__header");
 
 button.addEventListener("click", () => {
-   if (footer__header.innerText === "e-mail") {
-      footer__header.innerText = "maja@poczta.pl";
-   } else {
-      (footer__header.innerText === "maja@poczta.pl")
-      footer__header.innerText = "e-mail";
-
-   } if (button.innerText === "Pokaż") {
-      button.innerText = "Ukryj";
-   } else {
-      (button.innerText === "Ukryj")
-      button.innerText = "Pokaż";
-
-   }
+  if (footer__header.innerText === "e-mail") {
+    footer__header.innerText = "maja@poczta.pl";
+  } else {
+    footer__header.innerText === "maja@poczta.pl";
+    footer__header.innerText = "e-mail";
+  }
+  if (button.innerText === "Pokaż") {
+    button.innerText = "Ukryj";
+  } else {
+    button.innerText === "Ukryj";
+    button.innerText = "Pokaż";
+  }
 });
+//
+
 
 
