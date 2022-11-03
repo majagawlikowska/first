b4ce141eb587fa6b018d326dc3c580caf01df5af
Maja Gawlikowska <majgaw@onet.eu>
2022-11-03 09:00:23 +0100

    js changed

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 console.log("Hi there, (❁´◡`❁)");
 
 {
-const TaggleEmail = () => {
-   footer__header.innerText ("e-mail") === button.innerText ("Pokaż") = footer__header.innerText ("maja@poczta.pl") === button.innerText ("Ukryj")
-   footer__header.innerText ("maja@poczta.pl") === button.innerText ("Ukryj") = footer__header.innerText ("e-mail") === button.innerText ("Pokaż")
-}
+   const TaggleEmail = () => {
+      footer__header.innerText("e-mail") === button.innerText("Pokaż") ? footer__header.innerText("maja@poczta.pl") === button.innerText("Ukryj");
+      footer__header.innerText("maja@poczta.pl") === button.innerText("Ukryj") ? footer__header.innerText("e-mail") === button.innerText("Pokaż");
+   }
 
-const init = () => {
-button = document.querySelector(".button");
-button.addEventListener ("click", TaggleEmail);
+   const init = () => {
+      button = document.querySelector(".button");
+      button.addEventListener("click", TaggleEmail);
+      init ();
 
-}
+   }
 }
 
 
