5e8393e1465f8bae6f4d076a5248f7507352d284
Maja Gawlikowska <majgaw@onet.eu>
2022-11-04 07:19:30 +0100

    JS modified

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@ console.log("Hi there, (❁´◡`❁)");
 
 {
    const TaggleEmail = () => {
+
+      
       footer__header.innerText("e-mail") === button.innerText("Pokaż") = footer__header.innerText("maja@poczta.pl") === button.innerText("Ukryj");
       footer__header.innerText("maja@poczta.pl") === button.innerText("Ukryj") = footer__header.innerText("e-mail") === button.innerText("Pokaż");
    }
 
    const init = () => {
+
       button = document.querySelector(".button");
       button.addEventListener("click", TaggleEmail);
-      init ();
-
    }
+
+   init();
 }
 
 
