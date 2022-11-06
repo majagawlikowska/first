9400e8ff746bcc6177c91edacf047c115764e8db
Maja Gawlikowska <majgaw@onet.eu>
2022-11-06 08:05:33 +0100

    js changed

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ let button = document.querySelector(".button");
 let footer__header = document.querySelector(".footer__header");
 
 button.addEventListener("click", () => {
-  if (footer__header.innerText === "e-mail") {
-    footer__header.innerText = "maja@poczta.pl";
-  } else {
-    footer__header.innerText === "maja@poczta.pl";
-    footer__header.innerText = "e-mail";
-  }
-  if (button.innerText === "Pokaż") {
-    button.innerText = "Ukryj";
-  } else {
-    button.innerText === "Ukryj";
-    button.innerText = "Pokaż";
-  }
+   if (footer__header.innerText === "e-mail") {
+      footer__header.innerText = "maja@poczta.pl";
+   } else {
+      footer__header.innerText === "maja@poczta.pl";
+      footer__header.innerText = "e-mail";
+   }
+   if (button.innerText === "Pokaż") {
+      button.innerText = "Ukryj";
+   } else {
+      button.innerText === "Ukryj";
+      button.innerText = "Pokaż";
+   }
 });
 
 
